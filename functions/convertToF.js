8981e707d97eedac15a8d812cf48e1cce989359d
Diego Pérez <diegoperezcorreo@gmail.com>
2019-02-25 14:31:40 -0300

    Add to comment: function description, and examples.

--- functions/convertToF.js
+++ functions/convertToF.js
@@ -1,7 +1,25 @@
 /**
  * 
+ * @description       Convert from Celsius to Fahrenheit
  * @param   {number}  celsius 
  * @returns {number}  number 
+ * @example
+ *
+ * convertToF(0);   
+ * // =>  32
+ *
+ * convertToF(30);
+ * // =>  86
+ *
+ * convertToF(-30); 
+ * // => -22
+ *
+ * convertToF(-10); 
+ * // => 14
+ *
+ * convertToF(20);    
+ * // => 68
+ *
  */
 
 function convertToF(celsius) {
